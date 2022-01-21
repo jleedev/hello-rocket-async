44d0095d7ecb1f89d6035e03246f7700302a543e
Josh Lee <jleedev@gmail.com>
2022-01-21 16:30:30 +0000

    version bump

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate rocket;
 use rocket::http::uri::Origin;
 use rocket::http::{HeaderMap, Method};
 use rocket::request::{FromRequest, Outcome};
-use rocket::tokio::time::{delay_for, Duration};
+use rocket::tokio::time::{sleep, Duration};
 use rocket::Request;
 
 #[get("/")]
@@ -17,7 +17,7 @@ GET /echo
 
 #[get("/delay/<seconds>")]
 async fn delay(seconds: u64) -> String {
-    delay_for(Duration::from_secs(seconds)).await;
+    sleep(Duration::from_secs(seconds)).await;
     format!("Waited for {} seconds\n", seconds)
 }
 
@@ -28,23 +28,23 @@ async fn env() -> String {
         .collect()
 }
 
-struct AllHeaders<'a, 'r>(&'a HeaderMap<'r>);
+struct AllHeaders<'r>(&'r HeaderMap<'r>);
 #[rocket::async_trait]
-impl<'a, 'r> FromRequest<'a, 'r> for AllHeaders<'a, 'r> {
+impl<'r> FromRequest<'r> for AllHeaders<'r> {
     type Error = std::convert::Infallible;
-    async fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
         Outcome::Success(Self(request.headers()))
     }
 }
 
 #[get("/echo")]
-async fn echo(all_headers: AllHeaders<'_, '_>, origin: &Origin<'_>, method: Method) -> String {
+async fn echo(all_headers: AllHeaders<'_>, origin: &Origin<'_>, method: Method) -> String {
     let AllHeaders(hm) = all_headers;
     let headers: String = hm.iter().map(|h| format!("{}\n", h)).collect();
     format!("{} {}\n{}", method, origin, headers)
 }
 
 #[launch]
-fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index, delay, env, echo])
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, delay, env, echo])
 }
